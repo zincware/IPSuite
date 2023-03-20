e435416446a452fdaaba104ab059eea493293dcd
PythonFZ <fabian.zills@web.de>
2023-03-20 17:22:53 +0100

    try dscribe instead of tensorflow to check if module is avavailable

--- ipsuite/configuration_comparison/__init__.py
+++ ipsuite/configuration_comparison/__init__.py
@@ -4,6 +4,9 @@ import logging
 log = logging.getLogger(__name__)
 
 try:
+    # use as check if the Module is installed, before e.g. Tensorflow is loaded.
+    import dscribe  # noqa: F401
+
     from ipsuite.configuration_comparison.base import ConfigurationComparison
     from ipsuite.configuration_comparison.MMKernel import MMKernel
     from ipsuite.configuration_comparison.REMatch import REMatch
