46598f131cb9ad6802319b1cd71bd1b60d4f66cd
Moritz Schaefer <moritz.schaefer@pm.me>
2023-03-21 14:13:51 +0100

    xtb node outline

---
+++ ipsuite/calculators/xtb.py
@@ -0,0 +1,29 @@
+import logging
+
+import zntrack 
+
+log = logging.getLogger(__name__)
+
+
+class xTBCalc(zntrack.Node):
+    """Node for obtaining xTB calculators.
+
+    Attributes
+    ----------
+    method: str
+        xTB method to be used. 
+    """
+    method: str =  zntrack.zn.params("GFN1-xTB")
+
+    @property
+    def calc(self):
+        """Get an xtb ase calculator."""
+        try:
+            from xtb.ase.calculator import XTB
+        except ImportError:
+            log.warning(
+                "No xtb-python installation found. install via `conda install xtb-python`"
+            )
+
+        xtb = XTB(method=self.method)
+        return xtb
