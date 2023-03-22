7b305be451dc5e650a10d7ee810449d2f3083561
PythonFZ <fabian.zills@web.de>
2023-03-22 12:41:49 +0100

    data has been updated

--- tests/integration/test_i_geometry.py
+++ tests/integration/test_i_geometry.py
@@ -17,8 +17,8 @@ def test_ips_BarycenterMapping(data_repo, eager):
     if not eager:
         mapping.load()
 
-    assert len(mapping.atoms) == 20
-    assert len(mapping.molecules) == 20
+    assert len(mapping.atoms) == 30
+    assert len(mapping.molecules) == 30
     assert isinstance(mapping.atoms, list)
     assert isinstance(mapping.molecules, list)
     assert isinstance(mapping.molecules[0], list)
