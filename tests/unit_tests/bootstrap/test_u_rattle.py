4fd3c7c9488d064969ab9b25a7911ac69eae1860
Moritz Schaefer <moritz.schaefer@pm.me>
2023-03-22 16:49:26 +0100

    added unit test for Rattle Atoms

---
+++ tests/unit_tests/bootstrap/test_u_rattle.py
@@ -0,0 +1,35 @@
+import pathlib
+import shutil
+
+import pytest
+
+import ipsuite as ips
+
+
+@pytest.mark.parametrize("include_original", [True, False])
+def test_rattle_atoms(proj_path, traj_file, include_original):
+    traj_file = pathlib.Path(traj_file)
+    shutil.copy(traj_file, ".")
+
+    n_configs = 10
+
+    with ips.Project() as project:
+        data = ips.AddData(file=traj_file.name)
+
+        rattle = ips.bootstrap.RattleAtoms(
+            data=data.atoms,
+            n_configs=n_configs,
+            include_original=include_original,
+            seed=0,
+        )
+    project.run()
+
+    data.load()
+    rattle.load()
+    rattled_atoms = rattle.atoms
+
+    desired_num_configs = n_configs
+    if include_original:
+        desired_num_configs += 1
+
+    assert len(rattled_atoms) == desired_num_configs
