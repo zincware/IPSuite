908754f4954f0c6c6153007cf55425bdc8540c62
Moritz R. Schäfer <schaefer@theochem.uni-stuttgart.de>
2023-03-27 19:28:04 +0200

    added unit tests for translate and rotate molecules

--- tests/unit_tests/bootstrap/test_u_rattle.py
+++ tests/unit_tests/bootstrap/test_u_rattle.py
@@ -18,6 +18,67 @@ def test_rattle_atoms(proj_path, traj_file, include_original):
 
         rattle = ips.bootstrap.RattleAtoms(
             data=data.atoms,
+            range_maximum=0.1,
+            n_configs=n_configs,
+            include_original=include_original,
+            seed=0,
+        )
+    project.run()
+
+    data.load()
+    rattle.load()
+    rattled_atoms = rattle.atoms
+
+    desired_num_configs = n_configs
+    if include_original:
+        desired_num_configs += 1
+
+    assert len(rattled_atoms) == desired_num_configs
+
+
+@pytest.mark.parametrize("include_original", [True, False])
+def test_translate_molecules(proj_path, traj_file, include_original):
+    traj_file = pathlib.Path(traj_file)
+    shutil.copy(traj_file, ".")
+
+    n_configs = 10
+
+    with ips.Project() as project:
+        data = ips.AddData(file=traj_file.name)
+
+        rattle = ips.bootstrap.TranslateMolecules(
+            data=data.atoms,
+            range_maximum=0.1,
+            n_configs=n_configs,
+            include_original=include_original,
+            seed=0,
+        )
+    project.run()
+
+    data.load()
+    rattle.load()
+    rattled_atoms = rattle.atoms
+
+    desired_num_configs = n_configs
+    if include_original:
+        desired_num_configs += 1
+
+    assert len(rattled_atoms) == desired_num_configs
+
+
+@pytest.mark.parametrize("include_original", [True, False])
+def test_rotate_molecules(proj_path, traj_file, include_original):
+    traj_file = pathlib.Path(traj_file)
+    shutil.copy(traj_file, ".")
+
+    n_configs = 10
+
+    with ips.Project() as project:
+        data = ips.AddData(file=traj_file.name)
+
+        rattle = ips.bootstrap.RotateMolecules(
+            data=data.atoms,
+            range_maximum=0.1,
             n_configs=n_configs,
             include_original=include_original,
             seed=0,
