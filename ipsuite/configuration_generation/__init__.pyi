39b8ad9d303244a2df04a6e047c6ad05f9d572fd
PythonFZ <fabian.zills@web.de>
2023-04-08 21:00:43 +0200

    relative imports

--- ipsuite/configuration_generation/__init__.pyi
+++ ipsuite/configuration_generation/__init__.pyi
@@ -1,6 +1,6 @@
 """Module for generating new configurations based on smiles."""
 
-from ipsuite.configuration_generation.packmol import Packmol
-from ipsuite.configuration_generation.smiles_to_atoms import SmilesToAtoms
+from .packmol import Packmol
+from .smiles_to_atoms import SmilesToAtoms
 
 __all__ = ["SmilesToAtoms", "Packmol"]
