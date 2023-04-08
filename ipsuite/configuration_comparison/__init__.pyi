cb26740e60b25ebc045b9ef7f90ba8c4837e149b
PythonFZ <fabian.zills@web.de>
2023-04-08 12:40:10 +0200

    add lazy_loader

---
+++ ipsuite/configuration_comparison/__init__.pyi
@@ -0,0 +1,5 @@
+from ipsuite.configuration_comparison.base import ConfigurationComparison
+from ipsuite.configuration_comparison.MMKernel import MMKernel
+from ipsuite.configuration_comparison.REMatch import REMatch
+
+__all__ = ["MMKernel", "ConfigurationComparison", "REMatch"]
