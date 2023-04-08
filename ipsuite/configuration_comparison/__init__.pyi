da55e8055c3d169efec643accd06b80c91444187
PythonFZ <fabian.zills@web.de>
2023-04-08 12:45:02 +0200

    relative imports

--- ipsuite/configuration_comparison/__init__.pyi
+++ ipsuite/configuration_comparison/__init__.pyi
@@ -1,5 +1,5 @@
-from ipsuite.configuration_comparison.base import ConfigurationComparison
-from ipsuite.configuration_comparison.MMKernel import MMKernel
-from ipsuite.configuration_comparison.REMatch import REMatch
+from .base import ConfigurationComparison
+from .MMKernel import MMKernel
+from .REMatch import REMatch
 
 __all__ = ["MMKernel", "ConfigurationComparison", "REMatch"]
