e1daa08ca4773429ebb20e3d0c48c988772eb90c
Fabian Zills <46721498+PythonFZ@users.noreply.github.com>
2023-05-09 16:06:32 +0200

    use lazy loading (#102)

--- ipsuite/calculators/__init__.py
+++ ipsuite/calculators/__init__.py
@@ -1,18 +1,3 @@
-from ipsuite.calculators.apax_jax_md import ApaxJaxMD
-from ipsuite.calculators.ase_geoopt import ASEGeoOpt
-from ipsuite.calculators.ase_md import ASEMD, FixedSphereASEMD, LagevinThermostat
-from ipsuite.calculators.cp2k import CP2KSinglePoint, CP2KYaml
-from ipsuite.calculators.lj import LJSinglePoint
-from ipsuite.calculators.xtb import xTBSinglePoint
+import lazy_loader as lazy
 
-__all__ = [
-    "CP2KSinglePoint",
-    "CP2KYaml",
-    "ASEGeoOpt",
-    "ASEMD",
-    "FixedSphereASEMD",
-    "xTBSinglePoint",
-    "LJSinglePoint",
-    "LagevinThermostat",
-    "ApaxJaxMD",
-]
+__getattr__, __dir__, __all__ = lazy.attach_stub(__name__, __file__)
