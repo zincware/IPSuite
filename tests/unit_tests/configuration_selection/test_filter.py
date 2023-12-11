90fe016a07026118684a0f4cf5a388f694d3725a
Tetracarbonylnickel <segreto@theochem.uni-stuttgart.de>
2023-12-11 14:25:46 +0100

    introduced test for filter selection

---
+++ tests/unit_tests/configuration_selection/test_filter.py
@@ -0,0 +1,36 @@
+import numpy as np
+import pytest
+
+from ipsuite.configuration_selection import FilterOutlier
+
+
+@pytest.mark.parametrize(
+    "key, cutoff_type, direction, cutoffs",
+    [
+        ("forces", "direct", "both", [7, 13]),
+        ("forces", "direct", "both", None),
+        ("forces", "around_mean", "both", None),
+    ],
+)
+def test_get_selected_atoms(atoms_list, key, cutoff_type, direction, cutoffs):
+    for idx, atoms in enumerate(atoms_list):
+        atoms.calc.results[key] = [idx, 0, 0]
+
+    filter = FilterOutlier(
+        key=key,
+        cutoff_type=cutoff_type,
+        direction=direction,
+        data=None,
+        cutoffs=cutoffs,
+        threshold=0.4,
+    )
+
+    if "direct" in cutoff_type and cutoffs is None:
+        with pytest.raises(ValueError):
+            selected_atoms = filter.select_atoms(atoms_list)
+    else:
+        test_selection = [8, 9, 10, 11, 12]
+        selected_atoms = filter.select_atoms(atoms_list)
+        assert isinstance(selected_atoms, list)
+        assert len(set(selected_atoms)) == 5
+        assert selected_atoms == test_selection
