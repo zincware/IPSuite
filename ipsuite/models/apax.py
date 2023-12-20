86ee2ae22c6fef7e34137c3848f98b6f874ff572
Moritz R. Schäfer <schaefer@theochem.uni-stuttgart.de>
2023-12-20 10:47:21 +0100

    updated apax node to use experiment field and abs paths

--- ipsuite/models/apax.py
+++ ipsuite/models/apax.py
@@ -83,8 +83,8 @@ class Apax(MLModel):
             self._parameter = yaml.safe_load(pathlib.Path(self.config).read_text())
 
             custom_parameters = {
-                "directory": self.model_directory.as_posix(),
-                "experiment": "",
+                "directory": self.model_directory.resolve().as_posix(),
+                "experiment": "experiment",
                 "train_data_path": self.train_data_file.as_posix(),
                 "val_data_path": self.validation_data_file.as_posix(),
             }
@@ -106,7 +106,8 @@ class Apax(MLModel):
 
     def move_metrics(self):
         """Move the metrics to the correct directories for DVC"""
-        shutil.move(self.model_directory / self.metrics_epoch.name, self.metrics_epoch)
+        path = self.model_directory / "experiment" / self.metrics_epoch.name
+        shutil.move(path, self.metrics_epoch)
 
     def get_metrics_from_plots(self):
         """In addition to the plots write a model metric"""
@@ -128,7 +129,7 @@ class Apax(MLModel):
     def get_calculator(self, **kwargs):
         """Get an apax ase calculator"""
         with self.state.use_tmp_path():
-            return ASECalculator(model_dir=self.model_directory)
+            return ASECalculator(model_dir=self.model_directory / "experiment")
 
 
 class ApaxEnsemble(base.IPSNode):
