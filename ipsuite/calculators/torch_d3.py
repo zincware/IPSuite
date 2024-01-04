3085858eb7c39995a2d318ee020ba0a0c285903e
PythonFZ <fabian.zills@web.de>
2024-01-04 12:11:25 +0100

    add tests

--- ipsuite/calculators/torch_d3.py
+++ ipsuite/calculators/torch_d3.py
@@ -1,10 +1,13 @@
 import contextlib
 
 import ase
+from ase.calculators.calculator import PropertyNotImplementedError
 import torch
 import tqdm
 import zntrack
 from torch_dftd.torch_dftd3_calculator import TorchDFTD3Calculator
+from ase.calculators.singlepoint import SinglePointCalculator
+import numpy as np
 
 from ipsuite import base, fields
 from ipsuite.utils.ase_sim import freeze_copy_atoms
@@ -24,19 +27,31 @@ class TorchD3(base.ProcessAtoms):
         calc = self.get_calculator()
         self.atoms = []
         for atoms in tqdm.tqdm(self.get_data(), ncols=70):
+            
+            if atoms.calc is None:
+                atoms.calc = SinglePointCalculator(
+                    atoms,
+                    energy=0,
+                    forces=np.zeros_like(atoms.positions),
+                    stress=np.zeros((3, 3)),
+                )
+
             _atoms = freeze_copy_atoms(atoms)
 
             atoms.calc = calc
             energy = atoms.get_potential_energy()
             forces = atoms.get_forces()
-            stress = atoms.get_stress()
 
             with contextlib.suppress(KeyError):
                 _atoms.calc.results["energy"] += energy
             with contextlib.suppress(KeyError):
                 _atoms.calc.results["forces"] += forces
-            with contextlib.suppress(KeyError):
-                _atoms.calc.results["stress"] += stress
+
+            with contextlib.suppress(PropertyNotImplementedError):
+                # non periodic systems
+                stress = atoms.get_stress()
+                with contextlib.suppress(KeyError):
+                    _atoms.calc.results["stress"] += stress
 
             self.atoms.append(_atoms)
 
---
+++ tests/integration/calculators/test_torchd3.py
@@ -0,0 +1,63 @@
+import ipsuite as ips
+from ase.build import molecule
+import ase.io
+import pytest
+import numpy.testing as npt
+import numpy as np
+
+
+def test_d3(proj_path):
+    water = molecule("H2O")
+    ase.io.write("water.xyz", water)
+
+    with ips.Project() as proj:
+        data = ips.AddData(file="water.xyz", name="water")
+        d3 = ips.calculators.TorchD3(
+            data=data.atoms,
+            xc="pbe",
+            damping="bj",
+            cutoff=5,
+            abc=False,
+            cnthr=4,
+            dtype="float32",
+        )
+    
+    proj.run()
+
+    d3.load()
+    assert d3.atoms[0].get_potential_energy() == pytest.approx(-0.00978192157446211)
+    npt.assert_almost_equal(d3.atoms[0].get_forces(), [[0.0, 0.0, 9.5722440e-05], [0.0, -4.0598028e-05, -4.7861220e-05], [0.0, 4.0598028e-05, -4.7861220e-05]])
+
+
+def test_d3_existing_calc(proj_path):
+    water = molecule("H2O")
+    water.cell = [[10, 0, 0], [0, 10, 0], [0, 0, 10]]
+    water.center()
+    water.pbc = True
+    ase.io.write("water.xyz", water)
+
+    with ips.Project() as proj:
+        data = ips.AddData(file="water.xyz", name="water")
+        lj = ips.calculators.LJSinglePoint(data=data.atoms)
+        d3 = ips.calculators.TorchD3(
+            data=lj.atoms,
+            xc="pbe",
+            damping="bj",
+            cutoff=5,
+            abc=False,
+            cnthr=4,
+            dtype="float32",
+        )
+    
+    proj.run()
+
+    lj.load()
+    d3.load()
+
+    assert lj.atoms[0].get_potential_energy() == pytest.approx(1.772068860)
+    assert lj.atoms[0].get_potential_energy() - d3.atoms[0].get_potential_energy() == pytest.approx(0.00978192157)
+
+    assert not np.allclose(lj.atoms[0].get_forces()[0], d3.atoms[0].get_forces()[0])
+    assert not np.allclose(lj.atoms[0].get_stress()[0], d3.atoms[0].get_stress()[0])
+
+    
