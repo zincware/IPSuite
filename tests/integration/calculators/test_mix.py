f8311dd1e8625c141e4921cfd5cf169c1aade8c2
PythonFZ <fabian.zills@web.de>
2024-01-04 18:04:24 +0100

    test as external Node

--- tests/integration/calculators/test_mix.py
+++ tests/integration/calculators/test_mix.py
@@ -63,3 +63,26 @@ def test_mix_calculators(proj_path, traj_file):
 
         assert true_energy == d.get_potential_energy()
         npt.assert_almost_equal(true_forces, d.get_forces())
+
+def test_mix_calculator_external(proj_path, traj_file):
+    lj1 = ips.calculators.LJSinglePoint(data=None)
+    lj2 = ips.calculators.LJSinglePoint(data=None)
+
+    with ips.Project(automatic_node_names=True) as proj:
+        data = ips.AddData(traj_file)
+        lj3 = ips.calculators.LJSinglePoint(data=data.atoms)
+
+        mix1 = ips.calculators.MixCalculator(
+            data=data.atoms,
+            calculators=[lj1, lj2],
+            methods="mean",
+        )
+    
+    proj.run()
+
+    lj3.load()
+    mix1.load()
+
+    for a, b in zip(lj3.atoms, mix1.atoms):
+        assert a.get_potential_energy() == b.get_potential_energy()
+        npt.assert_almost_equal(a.get_forces(), b.get_forces())
