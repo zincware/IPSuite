03d7943f4c0db4d641276b32e326724527c24096
Julian Peters <uni@julian-peters.com>
2024-04-09 10:36:16 +0200

    add copy button to code segments in docs

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -20,7 +20,9 @@ import sys
 
 sys.path.insert(0, os.path.abspath("../../ipsuite/"))
 
-extensions = ["sphinx.ext.autodoc", "sphinx.ext.doctest"]
+extensions = ["sphinx.ext.autodoc", 
+              "sphinx.ext.doctest",
+              "sphinx_copybutton"]
 
 
 templates_path = ["_templates"]
