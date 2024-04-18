b286f98fe71ae333ee0496b1d36151512eef6636
PythonFZ <fabian.zills@web.de>
2024-04-18 23:15:31 +0200

    rename to `checks`

--- tests/unit_tests/calculators/test_u_ase_geoopt.py
+++ tests/unit_tests/calculators/test_u_ase_geoopt.py
@@ -53,7 +53,7 @@ def test_ase_geoopt(proj_path, cu_box):
             data=data.atoms,
             model=model,
             optimizer="FIRE",
-            checker_list=[check],
+            checks=[check],
             run_kwargs={"fmax": 0.05},
             maxstep=2,
             name="opt_max_step",
