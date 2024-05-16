2d8e19aacd6ee098f2a92eb654234980ea1b7bcc
Tetracarbonylnickel <segreto@theochem.uni-stuttgart.de>
2024-05-16 22:47:47 +0200

    flatten test

---
+++ tests/integration/test_list_flatten.py
@@ -0,0 +1,46 @@
+import ase.io
+import numpy as np
+import numpy.testing as npt
+from ase import units
+import zntrack as zn
+
+import ipsuite as ips
+from ipsuite.utils.ase_sim import get_density_from_atoms
+
+
+def test_ase_md(proj_path, cu_box):
+    atoms = []
+    for _ in range(5):
+        atoms.extend(cu_box)
+        
+    ase.io.write("cu_box.xyz", atoms)
+    checker = ips.analysis.TemperatureCheck()
+    thermostat = ips.calculators.LangevinThermostat(
+        time_step=1,
+        temperature=1,
+        friction=1,
+    )
+    rescale_box = ips.calculators.RescaleBoxModifier(cell=10)
+    temperature_ramp = ips.calculators.TemperatureRampModifier(temperature=300)
+    with ips.Project() as project:
+        data = ips.AddData(file="cu_box.xyz")
+        model = ips.calculators.EMTSinglePoint(data=data.atoms)
+        mapped_md = zn.apply(ips.calculators.ASEMD, method="map")(
+            data=data.atoms,
+            data_ids=[0, 1, 2],
+            model=model,
+            checks=[checker],
+            modifiers=[rescale_box, temperature_ramp],
+            thermostat=thermostat,
+            steps=30,
+            sampling_rate=1,
+            dump_rate=33,
+        )
+        flat_md = ips.base.Flatten(mapped_md.structures)
+
+    project.run()
+
+    mapped_md.load()
+    flat_md.load()
+    
+    assert len(mapped_md.atoms) == len(flat_md.atoms)
