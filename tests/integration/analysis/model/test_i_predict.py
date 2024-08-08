16425a82a5968dd7b9bd0cdeb8bb01a5e2b47d43
Moritz R Schäfer <schaefer@theochem.uni-stuttgart.de>
2024-08-08 16:25:20 +0200

    update to uncertainty metrics, fix for stability check (#304)
    * fixed apax get_calculator() not taking unused kwargs
    
    * fixed stability md counting stable runs as having 0 stable steps
    
    * made connectivity check more robust
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * added individual ensemble predictions to ase output
    
    * reworked calibration plot, added gaussianity plot
    
    * updated plot label
    
    * update conn check with status
    
    * removed duplicates
    
    * more robust defaults
    
    * fixed pearsonr not showing up in energy calibration
    
    * simplidfied stability check
    
    * stability analysis now writes on the fly instead of once at the end
    
    * refactored gaussianity figure
    
    * unified usage of of std vs variance
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * temprorarily disable class check in apaxjaxmd
    
    * removed pearson metric
    
    * added mistakenly removed parameters of check back to node
    
    * gaussianity plot for energies
    
    * added rll metric,fixed axis labels
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * update to gauss plot
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * removed fold gaussian
    
    * use uncertainty toolbox for UQ cal metrics
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * fix uncertainty slice for differently sized systems
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * update poetry lock
    
    * linting
    
    * lock update
    
    * linting
    
    * removed comment
    
    * removed leftover apax configs
    
    * added test added docs
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * added run to test
    
    * fixed  nll call error
    
    ---------
    
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>
    Co-authored-by: Moritz Schaefer <moritz.schaefer@pm.me>

---
+++ tests/integration/analysis/model/test_i_predict.py
@@ -0,0 +1,32 @@
+import ipsuite as ips
+
+
+def test_calibration(data_repo):
+    water = ips.data_loading.AddDataH5MD.from_rev(name="water")
+
+    with ips.Project(automatic_node_names=True) as project:
+        test_data = ips.configuration_selection.RandomSelection(
+            data=water.atoms, n_configurations=5
+        )
+
+        train_data = ips.configuration_selection.RandomSelection(
+            data=water.atoms,
+            n_configurations=5,
+            exclude_configurations=test_data.selected_configurations,
+        )
+
+        model1 = ips.models.GAP(data=train_data.atoms, soap={"n_max": 2})
+        model2 = ips.models.GAP(data=train_data.atoms, soap={"n_max": 3})
+
+        ensemble_model = ips.models.EnsembleModel(models=[model1, model2])
+
+        pred = ips.analysis.Prediction(
+            data=test_data.atoms,
+            model=ensemble_model,
+        )
+        calibration = ips.analysis.CalibrationMetrics(
+            x=test_data.atoms,
+            y=pred.atoms,
+        )
+
+    project.run(environment={"OPENBLAS_NUM_THREADS": "1"})
