cedc5e7ede7710391c508245c32ac91db3890c71
PythonFZ <fabian.zills@web.de>
2024-11-19 09:57:46 +0100

    update atoms.fields

--- ipsuite/fields/atoms.py
+++ ipsuite/fields/atoms.py
@@ -1,42 +1,50 @@
 """Lazy ASE Atoms loading."""
 
-import typing
 
-import h5py
-import znh5md
+import functools
+import znfields
+from zntrack.config import (
+    NOT_AVAILABLE,
+    ZNTRACK_CACHE,
+    ZNTRACK_FIELD_DUMP,
+    ZNTRACK_FIELD_LOAD,
+    ZNTRACK_FIELD_SUFFIX,
+    ZNTRACK_INDEPENDENT_OUTPUT_TYPE,
+    ZNTRACK_OPTION,
+    ZnTrackOptionEnum,
+)
+from zntrack.plugins import base_getter, plugin_getter
+from zntrack import Node
 import zntrack
+import pathlib
+import json
+import yaml
 
-from ipsuite import base
+CWD = pathlib.Path(__file__).parent.resolve()
 
 
-class Atoms(zntrack.Field):
-    """Store list[ase.Atoms] in an ASE database."""
+def _frames_getter(self: Node, name: str, suffix: str) -> None:
+    with self.state.fs.open((self.nwd / name).with_suffix(suffix), mode="rb") as f:
+        with h5py.File(f) as file:
+            return znh5md.IO(file_handle=file)[:]
 
-    dvc_option = "--outs"
-    group = zntrack.FieldGroup.RESULT
+def _frames_save_func(self: Node, name: str, suffix: str) -> None:
+    file = (self.nwd / name).with_suffix(suffix)
+    io = znh5md.IO(filename=file)
+    io.extend(getattr(self, name))
 
-    def __init__(self):
-        super().__init__(use_repr=False)
 
-    def get_files(self, instance: zntrack.Node) -> list:
-        return [(instance.nwd / f"{self.name}.h5").as_posix()]
 
-    def get_stage_add_argument(self, instance: zntrack.Node) -> typing.List[tuple]:
-        return [(self.dvc_option, file) for file in self.get_files(instance)]
-
-    def save(self, instance: zntrack.Node):
-        """Save value with ase.db.connect."""
-        atoms: base.ATOMS_LST = getattr(instance, self.name)
-        instance.nwd.mkdir(exist_ok=True, parents=True)
-        file = self.get_files(instance)[0]
-
-        db = znh5md.IO(filename=file)
-        db.extend(atoms)
-
-    def get_data(self, instance: zntrack.Node) -> base.protocol.ATOMS_LST:
-        """Get data from znh5md File."""
-        file_name = self.get_files(instance)[0]
-
-        with instance.state.fs.open(file_name, "rb") as f:
-            with h5py.File(f) as file:
-                return znh5md.IO(file_handle=file)[:]
+def Atoms(*, cache: bool = True, independent: bool = False, **kwargs) -> znfields.field:
+    kwargs["metadata"] = kwargs.get("metadata", {})
+    kwargs["metadata"][ZNTRACK_OPTION] = ZnTrackOptionEnum.OUTS
+    kwargs["metadata"][ZNTRACK_CACHE] = cache
+    kwargs["metadata"][ZNTRACK_INDEPENDENT_OUTPUT_TYPE] = independent
+    kwargs["metadata"][ZNTRACK_FIELD_LOAD] = functools.partial(
+        base_getter, func=_frames_getter
+    )
+    kwargs["metadata"][ZNTRACK_FIELD_DUMP] = _frames_save_func
+    kwargs["metadata"][ZNTRACK_FIELD_SUFFIX] = ".h5"
+    return znfields.field(
+        default=NOT_AVAILABLE, getter=plugin_getter, **kwargs, init=False
+    )
