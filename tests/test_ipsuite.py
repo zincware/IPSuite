e8c565a63b093ece0debb97a02124d0ddf787020
Fabian Zills <fzills@icp.uni-stuttgart.de>
2024-11-26 21:27:48 +0100

    fix more tests

--- tests/test_ipsuite.py
+++ tests/test_ipsuite.py
@@ -13,7 +13,7 @@ def test_version():
 def test_node_imports():
     """Test that all nodes are imported correctly."""
     for node in ips.__all__:
-        if node in ["__version__", "Project"]:
+        if node in ["__version__", "Project", "base"]:
             continue
         subclass = issubclass(getattr(ips, node), ips.base.IPSNode)
         dataclass = dataclasses.is_dataclass(getattr(ips, node))
