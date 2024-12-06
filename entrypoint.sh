93447075bdbdc67b3f02bf70d8bb4c5f053bbfa3
Fabian Zills <46721498+PythonFZ@users.noreply.github.com>
2024-12-06 15:17:33 +0100

    Update entrypoint.sh (#374)

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 set -e
 
-# Run the command passed to the container
-exec "$@"
+# Source any necessary environment configuration
+source /opt/tools/cp2k/tools/toolchain/install/setup
+
+# Check if arguments were passed; if not, open /bin/bash
+if [ "$#" -eq 0 ]; then
+    exec /bin/bash
+else
+    # Run the command passed to the container
+    exec "$@"
+fi
