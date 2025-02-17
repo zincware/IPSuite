97c845ba0e4dca7db1fd0efe51bad55c082e7feb
Fabian Zills <46721498+PythonFZ@users.noreply.github.com>
2025-02-17 18:02:41 +0100

    migrate to `uv` (#390)
    * ipsuite uv setup
    
    * fix zntrack update
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * update pre-commit hook
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * update install
    
    * reduce version, add keywords, test on 3.11
    
    * remove version check
    
    ---------
    
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>

--- ipsuite/fields/atoms.py
+++ ipsuite/fields/atoms.py
@@ -4,20 +4,18 @@ import pathlib
 
 import h5py
 import znh5md
-from zntrack import Node
-from zntrack.config import NOT_AVAILABLE, ZnTrackOptionEnum
-from zntrack.fields.base import field
+import zntrack
 
 CWD = pathlib.Path(__file__).parent.resolve()
 
 
-def _frames_getter(self: Node, name: str, suffix: str) -> None:
+def _frames_getter(self: zntrack.Node, name: str, suffix: str) -> None:
     with self.state.fs.open((self.nwd / name).with_suffix(suffix), mode="rb") as f:
         with h5py.File(f) as file:
             return znh5md.IO(file_handle=file)[:]
 
 
-def _frames_save_func(self: Node, name: str, suffix: str) -> None:
+def _frames_save_func(self: zntrack.Node, name: str, suffix: str) -> None:
     file = (self.nwd / name).with_suffix(suffix)
     io = znh5md.IO(filename=file)
     io.create_file()
@@ -25,11 +23,11 @@ def _frames_save_func(self: Node, name: str, suffix: str) -> None:
 
 
 def Atoms(*, cache: bool = True, independent: bool = False, **kwargs):
-    return field(
-        default=NOT_AVAILABLE,
+    return zntrack.field(
+        default=zntrack.NOT_AVAILABLE,
         cache=cache,
         independent=independent,
-        zntrack_option=ZnTrackOptionEnum.OUTS,
+        field_type=zntrack.FieldTypes.OUTS,
         dump_fn=_frames_save_func,
         suffix=".h5",
         load_fn=_frames_getter,
