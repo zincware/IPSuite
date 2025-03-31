108999307dca1016cce4a89da1ce97ec99881ed1
MrJulEnergy <uni@julian-peters.com>
2025-03-31 16:16:53 +0200

    Change params path to deps path

--- ipsuite/calculators/plumed.py
+++ ipsuite/calculators/plumed.py
@@ -18,7 +18,7 @@ class PlumedCalculator(ips.base.IPSNode):
     data_id: int = zntrack.params(-1)
     model: typing.Any = zntrack.deps()
 
-    input_script_path: str = zntrack.params_path(None)  # plumed.dat
+    input_script_path: str = zntrack.deps_path(None)  # plumed.dat
     input_string: str = zntrack.params(None)
 
     temperature_K: float = zntrack.params(None)  # in Kelvin! Important for Metadynamics
@@ -46,9 +46,7 @@ class PlumedCalculator(ips.base.IPSNode):
 
         # needed for ase units:
         units_string = f"""UNITS LENGTH=A TIME={1/(1000 * units.fs)} ENERGY={units.mol/units.kJ}"""
-        print_string = f"""PRINT ARG=d FILE={(self.plumed_directory / "COLVAR").as_posix()} STRIDE=1"""
         self.setup.insert(0, units_string)
-        self.setup.append(print_string)
         with open((self.plumed_directory / "setup.dat").as_posix(), "w") as file:
             for line in self.setup:
                 file.write(line + "\n")
@@ -57,11 +55,8 @@ class PlumedCalculator(ips.base.IPSNode):
         self.plumed_directory.mkdir(parents=True, exist_ok=True)
         (self.plumed_directory / "outs.txt").write_text("Lorem Ipsum")
 
-
     def get_calculator(self, directory: str = None):
-        print("2")
         self.check_input_instructions() # get setup instructions
-        print("3")
         return Plumed(
             calc=self.model.get_calculator(),
             atoms=self.data[self.data_id],
