c50aa1d0371fa31d9f4681519617bb74bf63faa3
Fabian Zills <fzills@icp.uni-stuttgart.de>
2025-05-15 12:49:35 +0200

    introduce CP2KModel

--- ipsuite/abc.py
+++ ipsuite/abc.py
@@ -1,6 +1,7 @@
 """Abstract base classes and type hints."""
 
 import typing as t
+from pathlib import Path
 
 from ase.calculators.calculator import Calculator
 
@@ -9,4 +10,4 @@ T = t.TypeVar("T", covariant=True)
 
 class NodeWithCalculator(t.Protocol[T]):
     """Any @dataclass, including zntrack.Node that provides a calculator."""
-    def get_calculator(self, **kwargs) -> Calculator: ...
+    def get_calculator(self, directory: str|Path|None = None,**kwargs) -> Calculator: ...
---
+++ ipsuite/models/cp2k.py
@@ -0,0 +1,67 @@
+import dataclasses
+import zntrack
+from pathlib import Path
+from ase.calculators.cp2k import CP2K
+import yaml
+from cp2k_input_tools.generator import CP2KInputGenerator
+import logging
+import os
+import shutil
+from unittest.mock import patch
+import functools
+import subprocess
+
+log = logging.getLogger(__name__)
+
+
+@dataclasses.dataclass
+class CP2KModel:
+    cmd: str | None = None
+    config: str | Path = zntrack.params_path()
+    files: list[str|Path] = zntrack.deps_path(default_factory=list)
+
+    def _update_cmd(self):
+        if self.cmd is None:
+            # Load from environment variable IPSUITE_CP2K_SHELL
+            try:
+                self.cmd = os.environ["IPSUITE_CP2K_SHELL"]
+                log.info(f"Using IPSUITE_CP2K_SHELL={self.cmd}")
+            except KeyError as err:
+                raise RuntimeError(
+                    "Please set the environment variable 'IPSUITE_CP2K_SHELL' or set the cp2k executable."
+                ) from err
+
+    def get_input_script(self):
+        """Return the input script."""
+        with Path(self.config).open("r") as file:
+            cp2k_input_dict = yaml.safe_load(file)
+
+        return "\n".join(CP2KInputGenerator().line_iter(cp2k_input_dict))
+
+    def get_calculator(self, directory: str|Path, index: int = 0, **kwargs) -> CP2K:
+        directory = Path(directory) / f"cp2k-{index}"
+        directory.mkdir(parents=True, exist_ok=True)
+        for file in self.files:
+            shutil.copy(file, directory)
+
+        patch(
+            "ase.calculators.cp2k.subprocess.Popen",
+            wraps=functools.partial(subprocess.Popen, cwd=directory),
+        ).start()
+
+        return CP2K(
+            command=self.cmd,
+            inp=self.get_input_script(),
+            basis_set=None,
+            basis_set_file=None,
+            max_scf=None,
+            cutoff=None,
+            force_eval_method=None,
+            potential_file=None,
+            poisson_solver=None,
+            pseudo_potential=None,
+            stress_tensor=True,
+            xc=None,
+            print_level=None,
+            label=f"cp2k-{index}",
+        )
