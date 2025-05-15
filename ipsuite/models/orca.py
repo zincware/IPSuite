cfee41ba57f28eee15584e827cce187b420db1c5
PythonFZ <fabian.zills@web.de>
2025-05-15 16:38:15 +0200

    add docs

--- ipsuite/models/orca.py
+++ ipsuite/models/orca.py
@@ -10,6 +10,42 @@ log = logging.getLogger(__name__)
 
 @dataclasses.dataclass
 class ORCAModel:
+    """ORCA ASE calculator model.
+
+    Parameters
+    ----------
+    simpleinput : str
+        The ORCA input string.
+        For example: "B3LYP def2-TZVP enGrad TightSCF" to 
+        compute the energy and forces of a system using
+        the B3LYP functional with the def2-TZVP basis set.
+        See [1]_ for more information.
+    blocks : str
+        The ORCA blocks string to select the number of processors
+        and other settings.
+        For example: "%pal nprocs 2 end".
+    cmd : str | None
+        The command to run ORCA.
+        If not set, the environment variable IPSUITE_ORCA_SHELL is used.
+
+    Examples
+    --------
+    >>> import ipsuite as ips
+    >>> project = ips.Project()
+    >>> orca = ips.ORCAModel(
+    ...     simpleinput="B3LYP def2-TZVP enGrad TightSCF",
+    ...     blocks="%pal nprocs 2 end",
+    ... )
+    >>> with project:
+    ...     water = ips.Smiles2Conformers(smiles="O", numConfs=100)
+    ...     ips.ApplyCalculator(
+    ...         data=water.frames,
+    ...         model=orca,
+    ...     )
+    >>> project.build()
+
+    .. [1] https://orca-manual.mpi-muelheim.mpg.de/index.html
+    """
     simpleinput: str = "B3LYP def2-TZVP enGrad TightSCF"
     blocks: str = "%pal nprocs 2 end"
     cmd: str | None = None
