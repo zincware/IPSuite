42bf0349b90ca0bef7afa017a8f607a5a9df4203
Fabian Zills <fzills@icp.uni-stuttgart.de>
2025-06-13 09:58:29 +0200

    use `frames_path`

--- ipsuite/configuration_generation/smiles_to_atoms.py
+++ ipsuite/configuration_generation/smiles_to_atoms.py
@@ -1,8 +1,11 @@
 import ase
 import rdkit2ase
 import zntrack
+from pathlib import Path
 
 from ipsuite import base, fields
+import znh5md
+import h5py
 
 
 class Smiles2Atoms(base.IPSNode):
@@ -24,13 +27,13 @@ class Smiles2Atoms(base.IPSNode):
 
 
 class Smiles2Conformers(base.IPSNode):
-    frames: list[ase.Atoms] = fields.Atoms()
-
     smiles: str = zntrack.params()
     numConfs: int = zntrack.params()
     seed: int = zntrack.params(42)
     maxAttempts: int = zntrack.params(1000)
     cell: float = zntrack.params(100)
+
+    frames_path: Path = zntrack.outs_path(zntrack.nwd / "frames.h5")
 
     def run(self):
         frames = rdkit2ase.smiles2conformers(
@@ -43,4 +46,12 @@ class Smiles2Conformers(base.IPSNode):
             for atoms in frames:
                 atoms.set_cell([self.cell, self.cell, self.cell])
                 atoms.center()
-        self.frames = frames
+        
+        io = znh5md.IO(filename=self.frames_path)
+        io.extend(frames)
+
+    @property
+    def frames(self) -> list[ase.Atoms]:
+        with self.state.fs.open(self.frames_path, "rb") as f:
+            with h5py.File(f) as file:
+                return znh5md.IO(file_handle=file)[:]
