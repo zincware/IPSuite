687e277d04766f76674628b909502ee3ca3b9dca
Fabian Zills <fzills@icp.uni-stuttgart.de>
2025-06-13 10:13:19 +0200

    ignore C901

--- ipsuite/calculators/ase_geoopt.py
+++ ipsuite/calculators/ase_geoopt.py
@@ -42,7 +42,7 @@ class ASEGeoOpt(base.IPSNode):
 
     traj_file: pathlib.Path = zntrack.outs_path(zntrack.nwd / "structures.h5")
 
-    def run(self):
+    def run(self):  # noqa: C901
         if self.checks is None:
             self.checks = []
         if self.constraints is None:
