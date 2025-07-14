f9e1bb8a698cb235ecbe7da13d5ff9f0a6f11478
Fabian Zills <fzills@icp.uni-stuttgart.de>
2025-07-14 14:56:12 +0200

    include an image of the smiles with the nodes

--- ipsuite/configuration_generation/smiles_to_atoms.py
+++ ipsuite/configuration_generation/smiles_to_atoms.py
@@ -5,26 +5,40 @@ import h5py
 import rdkit2ase
 import znh5md
 import zntrack
+from rdkit import Chem
+from rdkit.Chem import Draw
 
-from ipsuite import base, fields
+from ipsuite import base
 
 
 class Smiles2Atoms(base.IPSNode):
-    frames: list[ase.Atoms] = fields.Atoms()
-
     smiles: str = zntrack.params()
-    cell: float = zntrack.params(None)
     seed: int = zntrack.params(1234)
+
+    frames_path: Path = zntrack.outs_path(zntrack.nwd / "frames.h5")
+    molecule_image_path: Path = zntrack.outs_path(zntrack.nwd / "molecule.png")
+
 
     def run(self):
         atoms = rdkit2ase.smiles2atoms(
             smiles=self.smiles,
             seed=self.seed,
         )
-        if self.cell:
-            atoms.set_cell([self.cell, self.cell, self.cell])
-            atoms.center()
-        self.frames = [atoms]
+
+        io = znh5md.IO(filename=self.frames_path)
+        io.append(atoms)
+
+        # Generate and save molecule image
+        mol = rdkit2ase.ase2rdkit(atoms)
+        img = Draw.MolToImage(mol, size=(300, 300))
+        img.save(self.molecule_image_path)
+
+    @property
+    def frames(self) -> list[ase.Atoms]:
+        with self.state.fs.open(self.frames_path, "rb") as f:
+            with h5py.File(f) as file:
+                return znh5md.IO(file_handle=file)[:]
+
 
 
 class Smiles2Conformers(base.IPSNode):
@@ -32,9 +46,9 @@ class Smiles2Conformers(base.IPSNode):
     numConfs: int = zntrack.params()
     seed: int = zntrack.params(42)
     maxAttempts: int = zntrack.params(1000)
-    cell: float = zntrack.params(100)
 
     frames_path: Path = zntrack.outs_path(zntrack.nwd / "frames.h5")
+    molecule_image_path: Path = zntrack.outs_path(zntrack.nwd / "molecule.png")
 
     def run(self):
         frames = rdkit2ase.smiles2conformers(
@@ -43,13 +57,15 @@ class Smiles2Conformers(base.IPSNode):
             randomSeed=self.seed,
             maxAttempts=self.maxAttempts,
         )
-        if self.cell:
-            for atoms in frames:
-                atoms.set_cell([self.cell, self.cell, self.cell])
-                atoms.center()
 
         io = znh5md.IO(filename=self.frames_path)
         io.extend(frames)
+        
+        # Generate and save molecule image using first conformer
+        if frames:
+            mol = rdkit2ase.ase2rdkit(frames[0])
+            img = Draw.MolToImage(mol, size=(300, 300))
+            img.save(self.molecule_image_path)
 
     @property
     def frames(self) -> list[ase.Atoms]:
