c40dc4b3c984b758386300fca7dac9be031d17b7
TobiasDijkhuis <tobiasdijkhuis@hotmail.com>
2025-12-18 11:09:15 +0100

    Fixed __init__ using mkinit

--- ipsuite/__init__.py
+++ ipsuite/__init__.py
@@ -1,11 +1,50 @@
 """The ipsuite package."""
 
-import lazy_loader as lazy
-
+# <AUTOGEN_INIT>
 from ipsuite.utils.helpers import setup_ase
 from ipsuite.utils.logs import setup_logging
 
-__getattr__, __dir__, __all__ = lazy.attach_stub(__name__, __file__)
-
 setup_logging(__name__)
 setup_ase()
+
+# fmt: off
+# <AUTOGEN_INIT>
+import lazy_loader
+
+
+__getattr__, __dir__, __all__ = lazy_loader.attach_stub(__name__, __file__)
+
+__all__ = ['ASEMD', 'ASEMDSafeSampling', 'ATOMS_LST', 'AddData', 'AddDataH5MD',
+           'AllowedStructuresFilter', 'AnalyseAtoms', 'AnalyseDensity',
+           'AnalyseGlobalForceSensitivity', 'AnalyseSingleForceSensitivity',
+           'AnalyseStructureMeanForce', 'ApplyCalculator', 'Atoms',
+           'BarycenterMapping', 'Berendsen', 'BondStretchAnalyses',
+           'BoxHeatUp', 'BoxOscillatingRampModifier', 'BoxScale',
+           'CalibrationMetrics', 'Check', 'CollectMDSteps',
+           'ComparePredictions', 'ConfigurationSelection', 'ConnectivityCheck',
+           'DebugCheck', 'DensityCheck', 'DipoleHistogram', 'EnergyHistogram',
+           'EnergySpikeCheck', 'EnergyUncertaintyHistogram', 'FilterOutlier',
+           'FixedBondLengthConstraint', 'FixedLayerConstraint',
+           'FixedSphereConstraint', 'Flatten', 'ForceAngles',
+           'ForceDecomposition', 'ForceUncertaintyDecomposition',
+           'ForcesHistogram', 'ForcesUncertaintyHistogram', 'HasAtoms',
+           'HasOrIsAtoms', 'HasSelectedConfigurations', 'HookeanConstraint',
+           'IPSNode', 'IndexSelection', 'LangevinThermostat', 'MD22Dataset',
+           'MDStability', 'MoveSingleParticle', 'NPTThermostat', 'NaNCheck',
+           'NodeWithCalculator', 'NodeWithThermostat', 'Prediction',
+           'PredictionMetrics', 'PressureRampModifier', 'ProcessAtoms',
+           'ProcessSingleAtom', 'Project', 'RandomSelection', 'RattleAnalysis',
+           'RattleAtoms', 'RescaleBoxModifier', 'RotateMolecules',
+           'STATIC_PATH', 'SVCRBarostat', 'SplitSelection', 'StressHistogram',
+           'SurfaceRasterMetrics', 'SurfaceRasterScan', 'TemperatureCheck',
+           'TemperatureOscillatingRampModifier', 'TemperatureRampModifier',
+           'ThresholdCheck', 'ThresholdSelection', 'TranslateMolecules',
+           'UNION_ATOMS_OR_ATOMS_LST', 'UniformArangeSelection',
+           'UniformEnergeticSelection', 'UniformTemporalSelection',
+           'VelocityVerletDynamic', 'WrapModifier', 'analysis', 'ase_sim',
+           'base', 'bootstrap', 'calc', 'calculators', 'combine',
+           'configuration_generation', 'configuration_selection', 'conftest',
+           'data_loading', 'datasets', 'docs', 'doctest_namespace', 'dynamics',
+           'fields', 'geometry', 'helpers', 'interfaces', 'log', 'md',
+           'metrics', 'models', 'nodes', 'project', 'static_data', 'utils',
+           'version']
--- ipsuite/__init__.pyi
+++ ipsuite/__init__.pyi
@@ -226,6 +226,269 @@ __all__ = [
     # Project
     "Project",
     # Calc
+]
+
+from . import analysis
+from . import base
+from . import bootstrap
+from . import calc
+from . import calculators
+from . import configuration_generation
+from . import configuration_selection
+from . import conftest
+from . import data_loading
+from . import datasets
+from . import dynamics
+from . import fields
+from . import geometry
+from . import interfaces
+from . import models
+from . import nodes
+from . import project
+from . import static_data
+from . import utils
+from . import version
+
+from .analysis import (
+    AllowedStructuresFilter,
+    AnalyseDensity,
+    AnalyseGlobalForceSensitivity,
+    AnalyseSingleForceSensitivity,
+    AnalyseStructureMeanForce,
+    BondStretchAnalyses,
+    BoxHeatUp,
+    BoxScale,
+    CalibrationMetrics,
+    CollectMDSteps,
+    DipoleHistogram,
+    EnergyHistogram,
+    EnergyUncertaintyHistogram,
+    ForceAngles,
+    ForceDecomposition,
+    ForceUncertaintyDecomposition,
+    ForcesHistogram,
+    ForcesUncertaintyHistogram,
+    MDStability,
+    MoveSingleParticle,
+    Prediction,
+    PredictionMetrics,
+    RattleAnalysis,
+    StressHistogram,
+)
+from .base import (
+    AnalyseAtoms,
+    Check,
+    ComparePredictions,
+    Flatten,
+    IPSNode,
+    ProcessAtoms,
+    ProcessSingleAtom,
+    interfaces,
+)
+from .bootstrap import (
+    RattleAtoms,
+    RotateMolecules,
+    SurfaceRasterMetrics,
+    SurfaceRasterScan,
+    TranslateMolecules,
+)
+from .calc import (
+    ApplyCalculator,
+)
+from .configuration_selection import (
+    ConfigurationSelection,
+    FilterOutlier,
+    IndexSelection,
+    RandomSelection,
+    SplitSelection,
+    ThresholdSelection,
+    UniformArangeSelection,
+    UniformEnergeticSelection,
+    UniformTemporalSelection,
+)
+from .conftest import (
+    doctest_namespace,
+    project,
+)
+from .data_loading import (
+    AddData,
+    AddDataH5MD,
+)
+from .datasets import (
+    MD22Dataset,
+)
+from .dynamics import (
+    ASEMD,
+    ASEMDSafeSampling,
+    Berendsen,
+    BoxOscillatingRampModifier,
+    ConnectivityCheck,
+    DebugCheck,
+    DensityCheck,
+    EnergySpikeCheck,
+    FixedBondLengthConstraint,
+    FixedLayerConstraint,
+    FixedSphereConstraint,
+    HookeanConstraint,
+    LangevinThermostat,
+    NPTThermostat,
+    NaNCheck,
+    PressureRampModifier,
+    RescaleBoxModifier,
+    SVCRBarostat,
+    TemperatureCheck,
+    TemperatureOscillatingRampModifier,
+    TemperatureRampModifier,
+    ThresholdCheck,
+    VelocityVerletDynamic,
+    WrapModifier,
+)
+from .fields import (
+    Atoms,
+)
+from .geometry import (
+    BarycenterMapping,
+)
+from .interfaces import (
+    ATOMS_LST,
+    HasAtoms,
+    HasOrIsAtoms,
+    HasSelectedConfigurations,
+    NodeWithCalculator,
+    NodeWithThermostat,
+    ProcessAtoms,
+    UNION_ATOMS_OR_ATOMS_LST,
+)
+from .nodes import (
+    nodes,
+)
+from .project import (
+    Project,
+    log,
+)
+from .static_data import (
+    STATIC_PATH,
+)
+from .utils import (
+    ase_sim,
+    combine,
+    docs,
+    helpers,
+    md,
+    metrics,
+)
+
+__all__ = [
+    "ASEMD",
+    "ASEMDSafeSampling",
+    "ATOMS_LST",
+    "AddData",
+    "AddDataH5MD",
+    "AllowedStructuresFilter",
+    "AnalyseAtoms",
+    "AnalyseDensity",
+    "AnalyseGlobalForceSensitivity",
+    "AnalyseSingleForceSensitivity",
+    "AnalyseStructureMeanForce",
     "ApplyCalculator",
+    "Atoms",
+    "BarycenterMapping",
+    "Berendsen",
+    "BondStretchAnalyses",
+    "BoxHeatUp",
+    "BoxOscillatingRampModifier",
+    "BoxScale",
+    "CalibrationMetrics",
+    "Check",
+    "CollectMDSteps",
+    "ComparePredictions",
+    "ConfigurationSelection",
+    "ConnectivityCheck",
+    "DebugCheck",
+    "DensityCheck",
+    "DipoleHistogram",
+    "EnergyHistogram",
+    "EnergySpikeCheck",
+    "EnergyUncertaintyHistogram",
+    "FilterOutlier",
+    "FixedBondLengthConstraint",
+    "FixedLayerConstraint",
+    "FixedSphereConstraint",
+    "Flatten",
+    "ForceAngles",
+    "ForceDecomposition",
+    "ForceUncertaintyDecomposition",
+    "ForcesHistogram",
+    "ForcesUncertaintyHistogram",
+    "HasAtoms",
+    "HasOrIsAtoms",
+    "HasSelectedConfigurations",
+    "HookeanConstraint",
+    "IPSNode",
+    "IndexSelection",
+    "LangevinThermostat",
+    "MD22Dataset",
+    "MDStability",
+    "MoveSingleParticle",
+    "NPTThermostat",
+    "NaNCheck",
+    "NodeWithCalculator",
+    "NodeWithThermostat",
+    "Prediction",
+    "PredictionMetrics",
+    "PressureRampModifier",
+    "ProcessAtoms",
+    "ProcessSingleAtom",
+    "Project",
+    "RandomSelection",
+    "RattleAnalysis",
+    "RattleAtoms",
+    "RescaleBoxModifier",
+    "RotateMolecules",
+    "STATIC_PATH",
+    "SVCRBarostat",
+    "SplitSelection",
+    "StressHistogram",
+    "SurfaceRasterMetrics",
+    "SurfaceRasterScan",
+    "TemperatureCheck",
+    "TemperatureOscillatingRampModifier",
+    "TemperatureRampModifier",
+    "ThresholdCheck",
+    "ThresholdSelection",
+    "TranslateMolecules",
+    "UNION_ATOMS_OR_ATOMS_LST",
+    "UniformArangeSelection",
+    "UniformEnergeticSelection",
+    "UniformTemporalSelection",
+    "VelocityVerletDynamic",
     "WrapModifier",
+    "analysis",
+    "ase_sim",
+    "base",
+    "bootstrap",
+    "calc",
+    "calculators",
+    "combine",
+    "configuration_generation",
+    "configuration_selection",
+    "conftest",
+    "data_loading",
+    "datasets",
+    "docs",
+    "doctest_namespace",
+    "dynamics",
+    "fields",
+    "geometry",
+    "helpers",
+    "interfaces",
+    "log",
+    "md",
+    "metrics",
+    "models",
+    "nodes",
+    "project",
+    "static_data",
+    "utils",
+    "version",
 ]
